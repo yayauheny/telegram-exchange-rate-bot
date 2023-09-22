9788f1f42bd0bb5809ce30891f5ec2416afd9eec
yayauheny <work.evles@gmail.com>
2023-09-22 17:15:05 +0300

    initial commit

---
+++ src/main/java/by/yayauheny/exchangeratesbot/ExchangeRatesBotApplication.java
@@ -0,0 +1,13 @@
+package by.yayauheny.exchangeratesbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRatesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/yayauheny/exchangeratesbot/ExchangeRatesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yayauheny.exchangeratesbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
